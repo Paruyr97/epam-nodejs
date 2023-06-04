da6542127409f63754ceca9ca8128377d645b8d1
Paruyr-97 <p.avetisyan1997@gmail.com>
2023-06-04 18:07:09 +0400

    bugfix - create one transform stream for every operation

--- homework#6/app.js
+++ homework#6/app.js
@@ -2,34 +2,23 @@ import fs from 'fs';
 import { Transform } from 'stream';
 
 const operations = {
-    uppercase: new Transform({
-        transform(chunk, encoding, callback) {
-            callback(null, chunk.toString().toUpperCase());
-        },
-    }),
-    lowercase: new Transform({
-        transform(chunk, encoding, callback) {
-            callback(null, chunk.toString().toLowerCase());
-        },
-    }),
-    capitalize: new Transform({
-        transform(chunk, encoding, callback) {
-            callback(
-                null,
-                chunk
-                    .toString()
-                    .toLowerCase()
-                    .split(' ')
-                    .map(word => word.replace(word[0], word[0].toUpperCase()))
-                    .join(' ')
-            );
-        },
-    }),
-    reverse: new Transform({
-        transform(chunk, encoding, callback) {
-            callback(null, chunk.toString().split('').reverse().join(''));
-        },
-    }),
+    uppercase(str) {
+        return str.toString().toUpperCase()
+    },
+    lowercase(str) {
+        return str.toString().tolowercase()
+    },
+    capitalize(str) {
+        return str
+            .toString()
+            .toLowerCase()
+            .split(' ')
+            .map(word => word.replace(word[0], word[0].toUpperCase()))
+            .join(' ');
+    },
+    reverse(str) {
+        return str.toString().split('').reverse().join('');
+    }
 };
 
 function app(inputFile, outputFile, operation) {
@@ -48,7 +37,14 @@ function app(inputFile, outputFile, operation) {
     const fileStream = fs.createReadStream(inputFile);
     const transformedData = fs.createWriteStream(outputFile);
 
-    fileStream.pipe(operations[operation]).pipe(transformedData);
+    
+    fileStream
+    .pipe(new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, operations[operation](chunk));
+        },
+    }))
+    .pipe(transformedData);
 }
 
 const [inputFile, outputFile, operation] = process.argv.slice(2);
